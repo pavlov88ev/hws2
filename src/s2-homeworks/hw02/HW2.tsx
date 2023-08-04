ea705d1e9f8dee194101e99541bed84b8c6003ea
Eugene Pavlov <pavlov88ev@gmail.com>
2023-08-04 19:39:49 +0300

    homework 3

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -45,7 +45,6 @@ export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] =
 function HW2() {
     const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
-
     const filteredAffairs = filterAffairs(affairs, filter)
     const deleteAffairCallback = (_id: number) => { // need to fix any
         // need to fix
